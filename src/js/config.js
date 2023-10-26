cb38191688cfb99b9ab5b9526c30ebbc7fd4383c
Melvin Mathew <melvinmathew116@gmail.com>
2023-10-26 12:46:55 -0400

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '56b8850d-16d0-490b-91ec-a239ebaf15d3';
+export const MODAL_CLOSE_SEC = 2.5;
